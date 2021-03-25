4c8cafabb668f2f12645bffd41e0ee340610b3ea
Emerson Mellado <emersonmellado@gmail.com>
2021-03-24 19:10:39 -0700

    sharing images

---
+++ node-site-example/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    const superheroes = [
+        { name: 'SPIDER-MAN', image: 'spiderman.jpg' },
+        { name: 'CAPTAIN MARVEL', image: 'captainmarvel.jpg' },
+        { name: 'HULK', image: 'hulk.jpg' },
+        { name: 'THOR', image: 'thor.jpg' },
+        { name: 'IRON MAN', image: 'ironman.jpg' },
+        { name: 'DAREDEVIL', image: 'daredevil.jpg' },
+        { name: 'BLACK WIDOW', image: 'blackwidow.jpg' },
+        { name: 'CAPTAIN AMERICA', image: 'captainamerica.jpg' },
+        { name: 'WOLVERINE', image: 'wolverine.jpg' },
+    ];
+
+    res.render('index', { superheroes: superheroes });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
